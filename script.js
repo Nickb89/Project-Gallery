ce052edae2d71b1c861cd16d2c4a067c81790c1a
Nickb89 <nickb8989@gmail.com>
2023-01-26 23:20:31 +0000

    New feature-Collapsable description box and button
    I learned how to loop through my projects JSON file and created
    buttons, descriptions and links by inserting HTML elements.
    
    I looped through each button and added a 'click' event listener. When
    a button is clicked, the description's display is set to block instead
    of none. I then looped through each button to check that if it wasn't
    the current one selected then it should close (display: none).
    
    This has been lots of fun.

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
 fetch("imageInfo.json")
 .then(response => response.json())
 .then(data => {
-    console.log(data[0].url)
+
     let galleryContainer = document.getElementById("gallery");
     for (let i = 0; i < data.length; i++) {
-        let figure = document.createElement("figure");
-        figure.innerHTML = '<img src=' + data[i].url + '>' 
-        + '<figcaption><p>' + data[i].caption + '</p></figcaption>';
-        galleryContainer.appendChild(figure);
+        let itemDiv = document.createElement("div");
+        itemDiv.className = "itemDiv";
+        itemDiv.innerHTML = 
+        '<div class="textContainer"><button type="button" class="collapsible">' + data[i].caption + '</button>' + 
+        '<div class="description"><p>' +
+        data[i].description + '<a href="' + data[i].githubURL + '"> Link</a></p></div></div>' 
+        + '<img src=' + data[i].url + '>';
+        galleryContainer.appendChild(itemDiv);
     }
-})
+});
 
-   
+window.addEventListener('load', (event) => {
+  
+    let coll = document.getElementsByClassName("collapsible");
+    let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+    Array.from(document.querySelectorAll('.description')).forEach(el => {
+      if (el !== content)
+        el.style.display = 'none';
+    });
+ 
+  });
+}
+});
+
+
+// Fetch all the details element.
+const details = document.querySelectorAll("collapsible");
+
+// Add the onclick listeners.
+details.forEach((targetButton) => {
+  targetButton.addEventListener("click", () => {
+    // Close all the details that are not targetDetail.
+    details.forEach((detail) => {
+      if (detail !== targetDetail) {
+        detail.style.display = "none";
+      }
+    });
+  });
+});
+
 
 
