8ff4d6da0637ac3523bd0378abd0b8f425e50160
Nickb89 <nickb8989@gmail.com>
2023-02-02 18:11:41 +0000

    Added comments to code for better context.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Get JSON file and create html elements for each item in JSON list.
+
 fetch("imageInfo.json")
 .then(response => response.json())
 .then(data => {
@@ -9,29 +11,41 @@ fetch("imageInfo.json")
         itemDiv.innerHTML = 
         '<div class="textContainer"><button type="button" class="collapsible">' + data[i].caption + '</button>' + 
         '<div class="description"><p>' +
-        data[i].description + '<a href="' + data[i].githubURL + '"> Link</a></p></div></div>' 
+        data[i].description + '<a href="' + data[i].githubURL + '"><img src="images/arrow-right.png" width="12px" height="12px"></a></p></div></div>' 
         + '<img src=' + data[i].url + '>';
         galleryContainer.appendChild(itemDiv);
     }
-});
+}); 
+//learnt next time I should avoid innerHTML for security reasons.
+
+//Once document is loaded, get all of the newly added HTML elements.
 
 window.addEventListener('load', (event) => {
   
     let coll = document.getElementsByClassName("collapsible");
     let i;
 
+
+//add a click event listener to each button with class "collapsible"  
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
+    //change state to active
     this.classList.toggle("active");
+    //get the nextElementSibling which in this case is the description to expand when clicked.
     let content = this.nextElementSibling;
+    //if description is showing, set to dissapear, else show the description.
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
       content.style.display = "block";
     }
+    //select all of the description elements
+    //if the description doesn't match the clicked description it should not show and the buttons active class should be removed.
     Array.from(document.querySelectorAll('.description')).forEach(el => {
-      if (el !== content)
-        el.style.display = 'none';
+      if (el !== content){
+       el.style.display = 'none';
+       el.previousElementSibling.classList.remove("active");
+       } 
     });
  
   });
